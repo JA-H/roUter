dc9a7c95116848bfc5908d5a591aee85a96d59e5
kmanju8 <keshava249@gmail.com>
2021-04-22 17:22:17 +0100

    Fix polyline rendering on inputing new locations

--- router/src/components/Maps.js
+++ router/src/components/Maps.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import {useState} from 'react';
+//import {withScriptjs, withGoogleMap,GoogleMap, Polyline, Marker} from 'react-google-maps';
 
 import Marker from './MapMarker'
+import Polyline from './Polyline'
 
 //will be passing in different props later
 //polyline can technically be made into a component; may be worth considering later on ordering of loading but v. low priority.
 
 //props to take: polyline, ?attractions (to mark on map)
 function Maps({polyline}) {
+
+  const [mapVals, setMapVals] = useState({})
+
+  const onMapLoaded = (map, maps) => {
+    fitBounds(map, maps)
+
+    setMapVals({
+      mapsLoaded: true,
+      map: map,
+      maps: maps
+    })
+  }
 
   const decodedPathLongLat = window.google.maps.geometry.encoding.decodePath(polyline); 
   const decodedPath = decodedPathLongLat.map(x => x.toJSON());
@@ -24,6 +39,7 @@ function Maps({polyline}) {
   const distance = (lat1, lat2, lon1, lon2) => {
     let k = 111194.9;    // 2piR/360 (R is radius of earth)
     
+    
     return(
       k*Math.max(Math.abs(lat1-lat2),Math.abs(lon1-lon2))
     )
@@ -37,7 +53,8 @@ function Maps({polyline}) {
                             Math.min.apply(Math, decodedPath.map(function(o) { return o.lng; })),
                             Math.max.apply(Math, decodedPath.map(function(o) { return o.lng; }))
                           )
-    const zoomlvl = Math.floor(20-Math.log2((maxSpan/1000)/1128.5))
+    const zoomlvl = Math.floor(Math.log2(20-(maxSpan/1000)/1128.5))
+    console.log("zoom lvl is ", zoomlvl);
     
     if (zoomlvl>21){
       return(21)
@@ -51,39 +68,34 @@ function Maps({polyline}) {
   
   //Will need to turn to hooks later
   //may handle center and zoom as a function of polyline, so initially shows whole line.
-  
-  /* Zoom is a vague measure, may improve on but it's okay for now. Might force map to only focus on NY City*/
-  // let defaultProps = {
-  //     markers: decodedPath,
-  //     center: findCenter(decodedPath),
-  //     zoom: autoZoom(decodedPath)
-  // };
 
   
 
-  const renderPolylines = (map, maps) => {
-      /** Example of rendering geodesic polyline */
-      let geodesicPolyline = new maps.Polyline({
-        path: decodedPath,
-        geodesic: true,
-        strokeColor: '#00a1e1',
-        strokeOpacity: 1.0,
-        strokeWeight: 4
-      })
-      geodesicPolyline.setMap(map)
+  // const renderPolylines = (map, maps, path) => {
+
+
+  //     /** Example of rendering geodesic polyline */
+  //     let geodesicPolyline = new maps.Polyline({
+  //       path: path,
+  //       geodesic: true,
+  //       strokeColor: '#00a1e1',
+  //       strokeOpacity: 1.0,
+  //       strokeWeight: 4
+  //     })
+  //     geodesicPolyline.setMap(map)
   
-      /** Example of rendering non geodesic polyline (straight line) */
-      let nonGeodesicPolyline = new maps.Polyline({
-        path: decodedPath,
-        geodesic: false,
-        strokeColor: '#e4e4e4',
-        strokeOpacity: 0.7,
-        strokeWeight: 3
-      })
-      nonGeodesicPolyline.setMap(map)
+  //     /** Example of rendering non geodesic polyline (straight line) */
+  //     let nonGeodesicPolyline = new maps.Polyline({
+  //       path: path,
+  //       geodesic: false,
+  //       strokeColor: '#e4e4e4',
+  //       strokeOpacity: 0.7,
+  //       strokeWeight: 3
+  //     })
+  //     nonGeodesicPolyline.setMap(map)
   
-      fitBounds(map, maps)
-  }
+  //     fitBounds(map, maps, path)
+  // }
   
   const fitBounds = (map, maps) => {
     var bounds = new maps.LatLngBounds()
@@ -92,8 +104,22 @@ function Maps({polyline}) {
         new maps.LatLng(marker.lat, marker.lng)
       )
     }
+    console.log("actually running");
     map.fitBounds(bounds)
   }
+
+  const afterMapLoadChanges = () => {
+    return (
+      <div style={{display: 'none'}}>
+        <Polyline
+          map={mapVals.map}
+          maps={mapVals.maps}
+          markers={decodedPath} />
+      </div>
+    )
+  }
+
+
 
 
   return (
@@ -101,15 +127,26 @@ function Maps({polyline}) {
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyCzSDLwakvV-7nq3GXYc1sAapKFiAL8Fd4"}}
         center={findCenter(decodedPath)}
-        zoom={autoZoom(decodedPath)}
-        yesIWantToUseGoogleMapApiInternals={true}
-        onGoogleApiLoaded={({map, maps}) => renderPolylines(map, maps)}
+        zoom={17/*autoZoom(decodedPath)*/}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({map, maps}) => onMapLoaded(map, maps)}
       >
+        {/* <Polyline 
+          path={decodedPath}
+          options={{ 
+          strokeColor: '#0000ff',
+          strokeOpacity: 1,
+          strokeWeight: 2
+          }}
+          visible
+        /> */}
           <Marker text={'START'} lat={decodedPath[0].lat} lng={decodedPath[0].lng} />
           <Marker text={'END'} lat={decodedPath[decodedPath.length-1].lat} lng={decodedPath[decodedPath.length-1].lng} />
+          {mapVals.mapsLoaded ? afterMapLoadChanges() : ''}
       </GoogleMapReact>
     </div>
   )
+  
 }
 
 export default Maps
